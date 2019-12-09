e2f776d51fef33d39afd08047bc3bd25ae8a4653
Nandy <810411@cognizant.com>
2019-12-09 17:50:32 +0530

    commit

---
+++ medical/src/main/java/com/cognizant/medical/service/UserService.java
@@ -0,0 +1,9 @@
+package com.cognizant.medical.service;
+
+import com.cognizant.medical.entities.User;
+import com.cognizant.medical.exception.UserAlreadyExistsException;
+
+public interface UserService {
+	public void signUp(User user) throws UserAlreadyExistsException;
+	public boolean userExistingStatus(String username);
+}
